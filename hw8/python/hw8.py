208491d2bc62d0c7cfbb707c8a998ade0d5793a2
otakusaikou <otakuzyoutou@gmail.com>
2015-12-17 23:37:57 +0900

    Correct wrong assumption about point D, E, c1 and s5

--- hw8/python/hw8.py
+++ hw8/python/hw8.py
@@ -1,6 +1,6 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
-from sympy import symbols, Matrix, lambdify, S
+from sympy import symbols, Matrix, lambdify, solve, S
 import numpy as np
 import sys
 
@@ -15,10 +15,28 @@ def getEqn(Xp, Yp, xs, ls):
         [-((xs[6] - Xp[0])**2 + (xs[7] - Yp[0])**2)**.5 + ls[0]],
         [-((xs[4] - Xp[0])**2 + (xs[5] - Yp[0])**2)**.5 + ls[1]],
         [-((xs[4] - xs[2])**2 + (xs[5] - xs[3])**2)**.5 + ls[2]],
-        [-((xs[6] - xs[2])**2 + (xs[7] - xs[3])**2)**.5 + ls[3]],
-        [-((xs[2] - xs[0])**2 + (xs[3] - xs[1])**2)**.5 + ls[4]]])
+        [-((xs[6] - xs[2])**2 + (xs[7] - xs[3])**2)**.5 + ls[3]]])
 
     return F
+
+
+def getInit(Xp, Yp, l, xs):
+    """Compute initial values for unknown parameters"""
+    # Use known information as initial values of point B and C
+    Xb, Xc = Xp[1:]
+    Yb, Yc = Yp[1:]
+
+    # Compute Xd, Yd
+    Xd = Xp[2]
+    Yd = Yp[2] + Yp[0]
+
+    # Compute Xe, Ye
+    Ye = Xp[2] - Xp[1] + xs[1] - 8
+    eq = (xs[1] - 8)**2 + Ye**2 - l[3]**2
+    Xe = solve(eq)[-1]
+    Ye = Ye.subs(xs[1], Xe)
+
+    return (np.matrix([Xb, Yb, Xc, Yc, Xd, Yd, Xe, Ye]).T).astype(np.double)
 
 
 def getConstraint(xs, lcs):
@@ -31,26 +49,13 @@ def getConstraint(xs, lcs):
     return FC
 
 
-def getWcc(FuncC, x0, Wxx):
-    """Compute weight matrix of constraints"""
-    # Covariance matrix of unknown parameters
-    Qxx = Wxx.I
-
-    JFCx = np.matrix(FuncC(*x0)).astype(np.double)
-    Wcc = (JFCx * Qxx * JFCx.T).I
-
-    return Wcc
-
-
-def unified(Xp, Yp, l, W, Wxx, xs, ls):
+def unified(Xp, Yp, ls, l, W, xs, Wxx, sig0):
     """ Compute solution with unified LSQ method for case which has no other
     information"""
     print ("*" * 25 + "  Start  " + "*" * 25)
 
-    # Known information, coordinates of point D and E are inherited from hw7
-    lx = np.matrix(np.array(zip(Xp[1:], Yp[1:])).flatten()).T
-
-    # Use known information as initial values
+    # Get initial values
+    lx = getInit(Xp, Yp, l, xs)
     x0 = np.copy(lx)
     l0 = np.matrix(l).T
 
@@ -100,28 +105,29 @@ def unified(Xp, Yp, l, W, Wxx, xs, ls):
         # print "Iteration count: %d" % counter, u"|ΔX| = %.6f" % abs(X.sum())
         counter += 1    # Update Loop counter
 
-    # Compute residual vector
-    V = A.T * We * (f - B * X)
+    # Compute residual vectors for unknown parameters observations
+    Vx = np.array(x0 - lx + X)
 
     # Compute error of unit weight
     s0 = ((V.T * V)[0, 0] / (len(l0)))**0.5
 
     # Compute other information
     QXX = (N + Wxx).I
-    SigmaXX = s0**2 * QXX
+    SigmaXX = sig0**2 * QXX
     param_std = np.sqrt(np.diag(SigmaXX))
 
     # Output results
     print "Point coordinates:"
-    print ("%-8s"+" %-9s"*4) % ("Point", "X", "Y", "SD-X", "SD-Y")
-    print ("%-8s"+" %-9.6f"*4) % (
-        "Point B", x0[0], x0[1], param_std[0], param_std[1])
-    print ("%-8s"+" %-9.6f"*4) % (
-        "Point C", x0[2], x0[3], param_std[2], param_std[3])
-    print ("%-8s"+" %-9.6f"*4) % (
-        "Point D", x0[4], x0[5], param_std[4], param_std[5])
-    print ("%-8s"+" %-9.6f"*4) % (
-        "Point E", x0[6], x0[7], param_std[6], param_std[7])
+    print ("%-8s"+" %-9s"*6) % (
+        "Point", "X", "Y", "res-X", "res-Y", "SD-X", "SD-Y")
+    print ("%-8s"+" %-9.6f"*6) % (
+        "Point B", x0[0], x0[1], Vx[0], Vx[1], param_std[0], param_std[1])
+    print ("%-8s"+" %-9.6f"*6) % (
+        "Point C", x0[2], x0[3], Vx[2], Vx[3], param_std[2], param_std[3])
+    print ("%-8s"+" %-9.6f"*6) % (
+        "Point D", x0[4], x0[5], Vx[4], Vx[5], param_std[4], param_std[5])
+    print ("%-8s"+" %-9.6f"*6) % (
+        "Point E", x0[6], x0[7], Vx[6], Vx[7], param_std[6], param_std[7])
 
     print "\nMeasured distances:"
     print ("%-8s"+" %-9s"*2) % ("Distance", "value", "res")
@@ -134,23 +140,21 @@ def unified(Xp, Yp, l, W, Wxx, xs, ls):
     print "\nCofactor matrix:"
     print u"QΔΔ =".encode(LANG)
     print QXX
-    print ("*" * 26 + "  End  " + "*" * 26)
 
     print "\nStandard error of unit weight : %.4f" % s0
     print "Degree of freedom: %d" % (len(l0))
+    print ("*" * 26 + "  End  " + "*" * 26)
 
 
-def unifiedC(Xp, Yp, l, W, Wxx, xs, ls, lcs):
+def unifiedC(Xp, Yp, ls, l, W, xs, Wxx, lc, lcs, Wcc, sig0):
     """ Compute solution for case which which has constraint conditions with
     unified LSQ method for case which has no other information"""
     print ("*" * 25 + "  Start  " + "*" * 25)
 
-    # Known information, coordinates of point D and E are inherited from hw7
-    lx = np.matrix(np.array(zip(Xp[1:], Yp[1:])).flatten()).T
-
-    # Use known information as initial values
-    x0 = np.copy(lx)
-    l0 = np.matrix(l).T
+    l0 = np.matrix(l).T             # Initial values of range observations
+    lx = getInit(Xp, Yp, l, xs)     # Observation of unknown parameters
+    x0 = np.copy(lx)                # Initial values of unknown parameters
+    lc0 = np.matrix(lc).T           # Initial values of constraint observations
 
     # Get observation equations
     F = getEqn(Xp, Yp, xs, ls)
@@ -171,13 +175,6 @@ def unifiedC(Xp, Yp, l, W, Wxx, xs, ls, lcs):
     FuncAc = lambdify(tuple(xs), JFClc, modules='sympy')
     FuncC = lambdify(tuple(xs), JFCx, modules='sympy')
     FuncG = lambdify(tuple(np.append(xs, lcs)), FC, modules='sympy')
-
-    # Compute weight matrix of constraints
-    Wcc = getWcc(FuncC, x0, Wxx)
-
-    # Treat the constraints as observations and compute its value
-    lc = -np.matrix(FuncG(*tuple(np.append(x0, 0))))
-    lc0 = np.copy(lc)
 
     l = np.matrix(l).T                          # Observations with matrix form
     X = np.ones(1)                              # Initial value for iteration
@@ -233,28 +230,29 @@ def unifiedC(Xp, Yp, l, W, Wxx, xs, ls, lcs):
         # print "Iteration count: %d" % counter, u"|ΔX| = %.6f" % abs(X.sum())
         counter += 1    # Update Loop counter
 
-    # Compute residual vectors
-    V = A.T * We * (f - B * X)
+    # Compute residual vectors for unknown parameters observations
+    Vx = np.array(x0 - lx + X)
 
     # Compute error of unit weight
     s0 = np.double(((V.T * V)[0, 0] / (len(l0) + len(lc)))**0.5)
 
     # Compute other information
     QXX = (N + Nc + Wxx).I
-    SigmaXX = s0**2 * QXX
+    SigmaXX = sig0**2 * QXX
     param_std = np.sqrt(np.diag(SigmaXX))
 
     # Output results
     print "Point coordinates:"
-    print ("%-8s"+" %-9s"*4) % ("Point", "X", "Y", "SD-X", "SD-Y")
-    print ("%-8s"+" %-9.6f"*4) % (
-        "Point B", x0[0], x0[1], param_std[0], param_std[1])
-    print ("%-8s"+" %-9.6f"*4) % (
-        "Point C", x0[2], x0[3], param_std[2], param_std[3])
-    print ("%-8s"+" %-9.6f"*4) % (
-        "Point D", x0[4], x0[5], param_std[4], param_std[5])
-    print ("%-8s"+" %-9.6f"*4) % (
-        "Point E", x0[6], x0[7], param_std[6], param_std[7])
+    print ("%-8s"+" %-9s"*6) % (
+        "Point", "X", "Y", "res-X", "res-Y", "SD-X", "SD-Y")
+    print ("%-8s"+" %-9.6f"*6) % (
+        "Point B", x0[0], x0[1], Vx[0], Vx[1], param_std[0], param_std[1])
+    print ("%-8s"+" %-9.6f"*6) % (
+        "Point C", x0[2], x0[3], Vx[2], Vx[3], param_std[2], param_std[3])
+    print ("%-8s"+" %-9.6f"*6) % (
+        "Point D", x0[4], x0[5], Vx[4], Vx[5], param_std[4], param_std[5])
+    print ("%-8s"+" %-9.6f"*6) % (
+        "Point E", x0[6], x0[7], Vx[6], Vx[7], param_std[6], param_std[7])
 
     print "\nMeasured distances:"
     print ("%-8s"+" %-9s"*2) % ("Distance", "value", "res")
@@ -272,47 +270,56 @@ def unifiedC(Xp, Yp, l, W, Wxx, xs, ls, lcs):
     print "\nCofactor matrix:"
     print u"QΔΔ =".encode(LANG)
     print QXX
-    print ("*" * 26 + "  End  " + "*" * 26)
 
     print "\nStandard error of unit weight : %.4f" % s0
     print "Degree of freedom: %d" % (len(l0) + len(lc))
+    print ("*" * 26 + "  End  " + "*" * 26)
 
 
 def main():
     # Define input values
-    # Information of point D and E are inherited from hw7
-    Xp = np.array([0, 4, 8, 7.974993, 11.884163])
-    Yp = np.array([4, 0, 0, 3.955293, 7.845088])
-    l = np.array([12.41, 8.06, 3.87, 8.83, 4.0])
+    Xp = np.array([0, 4, 8])
+    Yp = np.array([4, 0, 0])
+    l = np.array([12.41, 8.06, 3.87, 8.83])
 
     # Define symbol arrays for unknown parameters and observations
-    # Point B, C, D and E are known point having error
+    # Point B, C are known point having error
     xs = np.array((symbols("Xb Yb Xc Yc Xd Yd Xe Ye")))
-
-    # Add new observation s5, s5 can be calculated from point B and C
-    ls = np.array(list(symbols("s1 s2 s3 s4 s5")))
+    ls = np.array(list(symbols("s1 s2 s3 s4")))
 
     # Define weight vector
-    # Error of s5 can be calculated from error of point B and C
-    w = np.array([.02, .02, .02, .02, .014142])
-    wxx = np.array([.01, .01, .01, .01, .140239, .139979, .17039, .186464])
+    w = np.array([.02, .02, .02, .02])
+    SigBC = .01         # Error in coordinates of point B and C
+    SigDE = 1000        # Error in coordinates of point D and E
+    wxx = np.append(np.ones(4) * SigBC, np.ones(4) * SigDE)
 
     sig0 = .02          # Define a priori error
-    # Define weight matrix for observations and unknown parameters
+
+    # Compute weight matrix for observations and unknown parameters
     W = np.matrix(np.diag(sig0**2 / w**2))
     Wxx = np.matrix(np.diag(sig0**2 / wxx**2))
 
     # For case which has no other information
     print "For case which has no other information..."
-    unified(Xp, Yp, l, W, Wxx, xs, ls)
+    unified(Xp, Yp, ls, l, W, xs, Wxx, sig0)
+
+    # Define constraint observations
+    lc = np.array([0])
 
     # Define symbol of constraint
     lcs = np.array([S("c1")])
 
+    # Weight of constraint
+    # Define weight vector of constraint
+    wcc = np.array([.002])
+
+    # Compute weight matrix for constraint
+    Wcc = np.matrix(np.diag(sig0**2 / wcc**2))
+
     # For case in which point B, D and E lie on a straight line
     # Without pseudo unknown parameters
     print "\nFor case in which point B, D and E lie on a straight line..."
-    unifiedC(Xp, Yp, l, W, Wxx, xs, ls, lcs)
+    unifiedC(Xp, Yp, ls, l, W, xs, Wxx, lc, lcs, Wcc, sig0)
 
     return 0
 
